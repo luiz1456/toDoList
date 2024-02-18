12078f2cefdebb9dddbed04218f842423768575c
luiz1456 <luizrodrigolimadesouza@gmail.com>
2024-02-18 14:23:27 -0300

    checkpoint

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-import {ConteudoTarefa} from './Pages/ConteudoTarefa/index.tsx'
+import { ConteudoTarefa } from './Pages/ConteudoTarefa/index.tsx'
 
 const router = createBrowserRouter([
   {
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-    <RouterProvider router={router} /> 
-  /* </React.StrictMode>, */
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
